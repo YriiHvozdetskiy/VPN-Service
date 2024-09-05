85666d9fb0969127a758fdaaad7aa5088fc3ccc6
rasty <hvozdetskiyyrii@gmail.com>
2024-09-05 19:21:50 +0300

    fix: fix STATIC_ROOT

--- djangoVpnService/settings.py
+++ djangoVpnService/settings.py
@@ -134,10 +134,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# Налаштування URL для статичних файлів
+STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+# Директорія, куди collectstatic збере всі статичні файли
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
